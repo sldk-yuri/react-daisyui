25f0812400102d03ccc5789378df38a3f83ff0fe
yurisldk <yurisldk@users.noreply.github.com>
2024-11-26 08:53:56 +0000

    docs: update storybook for latest release

---
+++ docs/assets/syntaxhighlighter-MJWPISIS-f15162d5.js
@@ -0,0 +1,1 @@
+import{S as c,c as l,s as n}from"./index-20d96dc5.js";import"./iframe-b833a08e.js";import"../sb-preview/runtime.js";import"./chunk-6E673XPF-4294b5bd.js";import"./index-f46741a2.js";import"./index-d868a670.js";import"./index-b7b0ffbb.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
